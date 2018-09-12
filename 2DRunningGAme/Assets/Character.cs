fb9310fc0df47bf8df7408aac764ec036cbfb80d
NoTabaco <32127313+NoTabaco@users.noreply.github.com>
2018-09-12 15:01:24 +0900

    Add Assets & ProjectSettings & Package
    Add

---
+++ 2DRunningGAme/Assets/Character.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour {
+
+    public float moveSpeed = 2f;
+    public float jumpPower = 300f;
+
+    public GameManager GameManager;
+
+    void Start()
+    {
+        Time.timeScale = 1f;    
+    }
+
+    // Update is called once per frame
+    void Update () {
+           // Update Frame 시간 차 * Time.timeScale * Time.deltaTime
+        transform.Translate (moveSpeed * Vector3.right * Time.deltaTime);
+	}
+
+    public void Jump()
+    {
+        GetComponent<Rigidbody2D>().AddForce(Vector3.up * jumpPower);
+    }
+    
+    void OnTriggerEnter2D(Collider2D col)
+    {
+        if(col.transform.name == "WinCollider")
+        {
+            GameManager.Win();
+        } else if(col.transform.name == "LoseCollider")
+        {
+            GameManager.Lose();
+        }
+    }
+}
---
+++ 2DRunningGAme/Assets/GameManager.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject winPanelObj, losePanelObj;
+
+	public void Win()
+    {
+        winPanelObj.SetActive(true);
+        Time.timeScale = 0;
+  //      Debug.Log("Win !");
+    }
+
+    public void Lose()
+    {
+        losePanelObj.SetActive(true);
+        Time.timeScale = 0;
+        //      Debug.Log("Lose !");
+    }
+
+    // 내가 Play한 것을 보여주는 게 아닌 Scene을 전환하여 다시 Play하도록 하게 해줌
+    public void Replay()
+    {
+        SceneManager.LoadScene("Play");
+    }
+}
